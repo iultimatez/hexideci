62038865b8d685738ebc60b15fb14103143ba1ea
iultimatez <appledynasty@gmail.com>
2014-05-11 01:04:31 +0800

    hexi add some deci

---
+++ app.js
@@ -0,0 +1,31 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function (cmd) {
+  //console.log('You just typed: '+cmd);
+  var toConvert = "";
+  var toAdd = "";
+  var flag =0;
+  for (var i = 0; i < cmd.length; i++) {
+  	if (cmd[i] === " ") {
+  		flag = 1;
+  	}else{
+  		if (flag === 0) {
+  			toConvert += cmd[i];
+  		}else{
+  			toAdd += cmd[i];
+  		}
+  	}
+  };
+  console.log("toConvert: " + toConvert + " toAdd: " + toAdd);
+  var deci = parseInt(toConvert, 16);
+  console.log("\n------------------------------------");
+  console.log("Input number in deci: " + deci);
+  deci += parseInt(toAdd);
+  console.log("Result: " + deci.toString(16));
+  console.log("------------------------------------\n");
+});
